b29fb46789990a4ca5c0deaa967a6ac977c41990
JAPV-X2612 <jesus.pinzon-v@mail.escuelaing.edu.co>
2025-12-03 12:36:20 -0500

    feat: Delete unused blank space

--- src/main/java/com/arsw/bomberdino/BomberDinoApplication.java
+++ src/main/java/com/arsw/bomberdino/BomberDinoApplication.java
@@ -13,7 +13,6 @@ public class BomberDinoApplication {
 
     /**
      * Main method to start the Spring Boot application.
-     *
      * @param args Command line arguments
      */
     public static void main(String[] args) {
